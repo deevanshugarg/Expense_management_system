54299ba9c2e33e6b2e156e3a47e983b94368ec7c
Deevanshu Garg <deevanshu@gmail.com>
2025-10-08 03:28:36 +0530

    backend links

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -10,7 +10,7 @@ const Register = () => {
   const submitHandler = async (values) => {
     try {
       setLoading(true);
-      await axios.post("/api/v1/users/register", values);
+      await axios.post("https://expense-management-system-zhcm.onrender.com/api/v1/users/register", values);
       message.success("Registeration Successfull");
       setLoading(false);
       navigate("/login");
